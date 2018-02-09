daaecb40dd4ae2adb49b4a75bdb4db085ed75b82
Manik Sewak <Maniks04@aol.com>
2018-02-08 18:41:29 -0600

    done

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import axios from 'axios'
 // import AnyComponent from './components/filename.jsx'
 
 class App extends React.Component {
   constructor(props) {
   	super(props)
   	this.state = {
-
-  	}
+      userName : '',
+      messages: [],
+      input : ''
+    }
+    this.serverPost = this.serverPost.bind(this)
+    this.appendMessage = this.appendMessage.bind(this)
+    this.serverGet=this.serverGet.bind(this)
   }
 
+  componentDidMount() {
+    console.log('mounted')
+    var userInput = window.prompt('TYPE USERNAME')
+    this.setState({userName : userInput})
+    this.serverGet()
+  }
+
+  serverGet () {
+    var context = this
+    axios.get('/chat/pat').then(function(res) {
+      context.appendMessage(res.data.results)
+      console.log(res)
+    }).catch(function(err) {
+      console.log('error')
+    })
+  }
+
+  appendMessage(array) {
+    this.setState({messages : array})
+  }
+
+
+  serverPost () {
+    var context = this
+    axios.post('/chat/pat',{
+      message: context.state.input,
+      user: this.state.userName
+    }).then(function(res) {
+      $('.input').val('')
+      console.log('response:', res)
+      context.appendMessage(res.data.results)
+    }).catch(function(err) {
+      console.log(err)
+    })
+  }
+
+  
+
   render () {
-  	return (<div>Hello World</div>)
+  	return (<div>
+              <div className="container">
+              <input className="input" placeholder="type here" onChange={(e) => this.setState({input: e.target.value})} onKeyUp={(e) => {if (e.keyCode=='13'){this.serverPost()}}}>
+              </input>
+              <button className="button" onClick={(e) => this.serverPost()}>submit</button>
+              </div>
+              <div id="chat">
+                {this.state.messages.map((message, i) => 
+                  <div className="text" key={i}>{message.user}: {message.message}</div>
+                )}
+              </div>
+
+            </div>
+    )
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -4,11 +4,24 @@ var request = require('request')
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
-// app.use(bodyParser.json())
+ app.use(bodyParser.json())
 
 // Due to express, when you load the page, it doesnt make a get request to '/', it simply serves up the dist folder
 app.post('/', function(req, res) {
   
+})
+const storage = {
+  results : []
+}
+app.post('/chat/pat', (req, res) => {
+  console.log(req.body)
+  storage.results.push({user:req.body.user, message: req.body.message})
+  res.end(JSON.stringify(storage))
+})
+
+app.get('/chat/pat', (req, res) => {
+  console.log(req.body)
+  res.end(JSON.stringify(storage))
 })
 
 app.listen(3000, function() {
